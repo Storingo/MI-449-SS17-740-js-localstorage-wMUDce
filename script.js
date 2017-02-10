4074ae61d9e2d9965677be7ec954b089a11853bc
Dylan O'Dowd <Dylan O'Dowd>
2017-02-10 15:36:09 -0500

    shortened visit count code, and checking and setting time of day if statments

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 var button1 = document.getElementById('button')
 
-var time = window.localStorage.getItem('timeOfDay')
-
 var visits = window.localStorage.getItem('numVisits')
 
-if (visits === null) {
-  visits = 1
-} else {
-  visits = parseInt(visits)
-  visits = visits + 1
-}
+visits = (parseInt(visits) || 0) + 1
+
 window.localStorage.setItem('numVisits', visits)
 document.getElementById('count').textContent = visits
 
-if (time === null) {
-  document.body.setAttribute('class', 'day-theme')
-} else {
-  document.body.setAttribute('class', time + '-theme')
-}
+var timeOfDay = window.localStorage.getItem('timeOfDay') || 'day'
+document.body.setAttribute('class', timeOfDay + '-theme')
 
 button1.addEventListener('click', function () {
-  var timeOfDay = window.localStorage.getItem('timeOfDay')
-  if (timeOfDay === null) {
-    timeOfDay = 'night'
-  } else if (timeOfDay === 'day') {
-    timeOfDay = 'night'
-  } else {
+  if (timeOfDay === 'night') {
     timeOfDay = 'day'
+  } else {
+    timeOfDay = 'night'
   }
   document.body.setAttribute('class', timeOfDay + '-theme')
   window.localStorage.setItem('timeOfDay', timeOfDay)
